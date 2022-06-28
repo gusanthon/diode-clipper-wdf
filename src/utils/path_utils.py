9fe1235cc0c43cacafff5dc4b5394a0c6f6fc610
Gus Anthon <anthon.gus1@gmail.com>
2022-06-28 03:03:10 +0200

    variable name changes

--- src/utils/path_utils.py
+++ src/utils/path_utils.py
@@ -4,7 +4,7 @@ utils_dir = Path(__file__).resolve().parent
 
 root_dir = utils_dir.parent.parent
 
-data_path = root_dir / "data"
-audio_path = root_dir / "audio"
-images_path = root_dir / "images"
-src_path = root_dir / "src"
+data_dir = root_dir / "data"
+audio_dir = root_dir / "audio"
+images_dir = root_dir / "images"
+src_dir = root_dir / "src"
