25d0fa0e809bc2408b205eb037db8adbaca9059c
Gus Anthon <anthon.gus1@gmail.com>
2022-08-03 12:24:52 -0400

    Added RType adaptor class

---
+++ src/Rtype.py
@@ -0,0 +1,94 @@
+from wdf import baseWDF, rootWDF
+import numpy as np
+
+
+class RTypeAdaptor(baseWDF):
+    
+    def __init__(self,down_ports,impedance_calc,up_port_idx):
+        baseWDF.__init__(self)
+        self.down_ports = down_ports
+        self.n_ports = len(down_ports) + 1
+        self.S_matrix = [[0 for x in range(self.n_ports)] for y in range(self.n_ports)]
+        self.impedance_calc = impedance_calc
+        self.a_vals = np.zeros(self.n_ports)
+        self.b_vals = np.zeros(self.n_ports)
+        self.up_port_idx = up_port_idx
+
+        [port.connect_to_parent(self) for port in self.down_ports]
+            
+        self.calc_impedance()
+
+    def accept_incident_wave(self, a):
+        self.a = a
+        self.a_vals[self.up_port_idx] = self.a
+        self.r_type_scatter()
+        for i in range(len(self.down_ports)):
+            idx = self.get_port_idx(i)
+            self.down_ports[i].accept_incident_wave(self.b_vals[idx])
+
+    def propagate_reflected_wave(self):
+        for i in range(len(self.down_ports)):
+            idx = self.get_port_idx(i) 
+            self.a_vals[idx] = self.down_ports[i].propagate_reflected_wave()
+        self.b = self.b_vals[self.up_port_idx]
+        return self.b
+
+    def get_port_idx(self,x):
+        return x if x < self.up_port_idx else x +1
+
+    def r_type_scatter(self):
+        for i in range(self.n_ports):
+            self.b_vals[i] = 0
+            for j in range(self.n_ports):
+                self.b_vals[i] += self.S_matrix[i][j] * self.a_vals[j]
+
+    def calc_impedance(self):
+        self.Rp = self.impedance_calc(self)
+        self.G = 1./self.Rp
+
+    def get_port_impedances(self):
+        return [port.Rp for port in self.down_ports]
+
+    def set_S_matrix(self,matrix):
+        for i in range(self.n_ports):
+            for j in range(self.n_ports):
+                self.S_matrix[i][j] = matrix[i][j]
+
+class RootRTypeAdaptor(rootWDF):
+
+    def __init__(self, down_ports, impedance_calc):
+        super().__init__()
+        self.down_ports = down_ports
+        self.n_ports = len(down_ports) 
+        self.S_matrix = [[0 for x in range(self.n_ports)] for y in range(self.n_ports)]
+        self.impedance_calc = impedance_calc
+        self.a_vals = np.zeros(self.n_ports)
+        self.b_vals = np.zeros(self.n_ports)
+
+        [port.connect_to_parent(self) for port in self.down_ports]
+
+        self.calc_impedance()
+
+    def compute(self):
+        self.r_type_scatter()
+        for i in range(len(self.down_ports)):
+            self.down_ports[i].accept_incident_wave(self.b_vals[i])
+            self.a_vals[i] = self.down_ports[i].propagate_reflected_wave() 
+
+    def r_type_scatter(self):
+        for i in range(self.n_ports):
+            self.b_vals[i] = 0
+            for j in range(self.n_ports):
+                self.b_vals[i] += self.S_matrix[i][j] * self.a_vals[j]
+
+    def calc_impedance(self):
+        self.impedance_calc(self)
+
+    def get_port_impedances(self):
+        return [port.Rp for port in self.down_ports]
+
+    def set_S_matrix(self,matrix):
+        for i in range(self.n_ports):
+            for j in range(self.n_ports):
+                self.S_matrix[i][j] = matrix[i][j]
+
